fbec8e598212feba57ee7989e5dcc43b32e98276
Yonghoon Lee <88873780+pooky1219@users.noreply.github.com>
2025-10-27 15:34:41 +0900

    Update term_project.js

--- term_project/term_project.js
+++ term_project/term_project.js
@@ -776,7 +776,7 @@ function animate() {
 
   // 플레이어 이동
   if (player && playerBody) {
-    const speed = 5;
+    const speed = 8;
     const rotationSpeed = 1.2;
 
     const isForward = keysPressed["ArrowUp"];
@@ -901,3 +901,4 @@ window.addEventListener("keydown", (e) => {
 window.addEventListener("keyup", (e) => {
   keysPressed[e.key] = false;
 });
+
